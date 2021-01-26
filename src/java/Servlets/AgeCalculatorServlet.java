c0058698056f71b33a69ec04aba4cc6aa0acb801
840018 <840018@SAIT225659IT.ACDM.DS.SAIT.CA>
2021-01-26 14:28:48 -0700

    Week3Lab_Calculators Initial commit for the week 3 lab

---
+++ src/java/Servlets/AgeCalculatorServlet.java
@@ -0,0 +1,39 @@
+
+package Servlets;
+
+import java.io.IOException;
+import java.io.PrintWriter;
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+/**
+ *
+ * @author 840018
+ */
+public class AgeCalculatorServlet extends HttpServlet {
+
+    /**
+     * Processes requests for both HTTP <code>GET</code> and <code>POST</code>
+     * methods.
+     *
+     * @param request servlet request
+     * @param response servlet response
+     * @throws ServletException if a servlet-specific error occurs
+     * @throws IOException if an I/O error occurs
+     */
+
+    
+    @Override
+    protected void doGet(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException {
+        
+    }
+
+    @Override
+    protected void doPost(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException {
+    }
+
+}
---
+++ web/WEB-INF/agecalculator.jsp
@@ -0,0 +1,17 @@
+<%-- 
+    Document   : agecalculator
+    Created on : Jan 26, 2021, 2:24:37 PM
+    Author     : 840018
+--%>
+
+<%@page contentType="text/html" pageEncoding="UTF-8"%>
+<!DOCTYPE html>
+<html>
+    <head>
+        <meta http-equiv="Content-Type" content="text/html; charset=UTF-8">
+        <title>Age Calculator</title>
+    </head>
+    <body>
+        <h1>The page works</h1>
+    </body>
+</html>
